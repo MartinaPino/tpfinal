f1ee6a38b862b96bcf2e40c708b5be8875f43200
priscilapratto <128637065+priscilapratto@users.noreply.github.com>
2023-11-20 18:29:37 -0300

    Add files via upload

---
+++ tpfinal/tpfinal.js
@@ -0,0 +1,22 @@
+//Pratto Priscila y Martina pino
+//tpfinal, comision 1
+
+
+let p;
+
+function setup() {
+  createCanvas(500,400);
+  p = new Principal();
+
+}
+
+
+function draw() {
+   background(8, 3, 3);
+   p.dibujar();
+
+}
+
+function keyPressed() {
+  p.teclaPresionada(keyCode);
+}
